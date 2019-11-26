1b8be83ad2af013b438dbd12f4fbda3a63f51646
Evan Jeffries <ejeffries14@gmail.com>
2019-11-26 06:29:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,8 @@ class School
     #@roster[grade] << student
     if @roster[grade]
       @roster[grade] << student
-    else
-      @roster[grade] = []
+    elsif @roster[grade] = []
+      @roster[grade] << student
     end
   end
 end
